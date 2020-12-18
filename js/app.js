4c2df316e2391ba8c580475e91c913784dc22f0b
Marcelo <sebastian.tenis.sc@gmail.com>
2020-12-18 09:51:52 -0300

    He creado la funcion para cambiar de color

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
-//VERIFICAR TABLERO
-function verificar_tablero() {
-	llenar_tablero();
+//CAMBIAR COLOR
+function cambiar_logo(selector) {
+	$(selector).animate({
+		opacity: '1',	},			 
+			{step: function () {	$(this).css('color', 'white');},
+			queue: true
+		}) .animate({
+			opacity: '1'}, 
+			{step: function () {	$(this).css('color', 'yellow');
+			},
+			queue: true
+		}, 500).delay(1000).animate({
+			opacity: '2'
+		}, {step: function () {    $(this).css('color', 'white');
+			},
+			queue: true
+		}) .animate({	opacity: '2'}, {step: function () {  $(this).css('color', 'yellow');
+			cambiar_logo('h1.main-titulo');
+			},
+			queue: true
+		});
 }
